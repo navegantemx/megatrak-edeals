3eea4c2c3e686e57e10b1720b1aa236e0955f785
navegantemx <raulvelazquez437@gmail.com>
2021-01-19 21:31:13 +0000

    Updated checkout form

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -81,7 +81,7 @@ class StripeWH_Handler:
                     original_cart=cart,
                     stripe_pid=pid,
                 )
-                for item_id, item_data in json.loads(bag).items():
+                for item_id, item_data in json.loads(cart).items():
                     product = Product.objects.get(id=item_id)
                     if isinstance(item_data, int):
                         order_line_item = OrderLineItem(
