60168042e2b6e9c3b44acb12c0daf0fda4375243
navegantemx <raulvelazquez437@gmail.com>
2021-01-19 15:08:17 +0000

    Added order summary to checkout page

--- checkout/migrations/0001_initial.py
+++ checkout/migrations/0001_initial.py
@@ -36,8 +36,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='OrderLineItem',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('product_size', models.CharField(blank=True, max_length=2, null=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),                
                 ('quantity', models.IntegerField(default=0)),
                 ('lineitem_total', models.DecimalField(decimal_places=2, editable=False, max_digits=6)),
                 ('order', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='lineitems', to='checkout.order')),
