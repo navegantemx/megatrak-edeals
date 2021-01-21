ed2ea03fda51d54c6b5d28d6758fa87c19dde315
navegantemx <raulvelazquez437@gmail.com>
2021-01-21 05:26:29 +0000

    Removed secret key and set debug

--- megatrak_edeals/settings.py
+++ megatrak_edeals/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nkf*zhbu&#x1omv@gnt+fch$#g7@tk#4hu_gi$pvb9r9k!8qp6'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['megatrak-edeals.herokuapp.com', 'localhost']
 
